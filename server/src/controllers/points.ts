894b1cbca4144425d379309cdaa1852704bf68cb
Maykon Oliveira <maykonoliveira850@gmail.com>
2020-06-03 14:46:40 -0300

    Refatoraçao

--- server/src/controllers/points.ts
+++ server/src/controllers/points.ts
@@ -11,39 +11,34 @@ PointController.post('', async (req, res) => {
 
     const trx = await db.transaction();
 
-    trx('points')
-        .insert({ ...point, image: 'default.svg' })
-        .then((points_id) => {
-            const point_items = items.map((item_id) => ({ item_id, point_id: points_id[0] }));
-            return trx('point_items').insert(point_items);
-        })
-        .then(() => res.status(201).json(point))
-        .catch((error) => {
-            console.log(error);
-            res.status(500).json(error);
-        });
+    const points_id = await trx('points').insert({ ...point, image: 'default.svg' });
+
+    const point_id = points_id[0];
+
+    const point_items = items.map((item_id) => ({ item_id, point_id }));
+
+    await trx('point_items').insert(point_items);
 
     await trx.commit();
+
+    return res.status(201).json({ ...point, point_id });
 });
 
-PointController.get('/:id', (req, res) => {
+PointController.get('/:id', async (req, res) => {
     const { id } = req.params;
 
-    db('points')
-        .where('id', id)
-        .first()
-        .then(async (point) => {
-            if (!point) {
-                return res.status(404).json();
-            }
-            const items = await db('items')
-                .join('point_items', 'items.id', '=', 'point_items.item_id')
-                .where('point_items.point_id', point.id)
-                .select('title', 'image');
+    const point = await db('points').where('id', id).first();
 
-            return res.json({ ...point, items });
-        })
-        .catch((err) => res.status(500).json(err));
+    if (!point) {
+        return res.status(404).json();
+    }
+
+    const items = await db('items')
+        .join('point_items', 'items.id', '=', 'point_items.item_id')
+        .where('point_items.point_id', point.id)
+        .select('title', 'image');
+
+    return res.json({ ...point, items });
 });
 
 PointController.get('', (req, res) => {
@@ -51,7 +46,7 @@ PointController.get('', (req, res) => {
 
     const sanalizedItems = String(items)
         .split(',')
-        .map((s) => Number(s.trim()));        
+        .map(Number);
 
     db('points')
         .join('point_items', 'points.id', '=', 'point_items.point_id')
